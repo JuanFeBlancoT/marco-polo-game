81b2845cf79d5efed44fe2b87669af473a2157d2
JuanFeBlancoT <72984915+JuanFeBlancoT@users.noreply.github.com>
2021-06-04 19:27:38 -0400

    Project initialization

---
+++ src/view/Main.java
@@ -0,0 +1,21 @@
+package view;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+	public static void main (String[] args) {
+		PApplet.main("view.Main");
+	}
+	
+	public void settings() {
+	size(400,400);
+	}
+	
+	public void setup() {
+
+	}
+
+	public void draw() {
+
+	}
+} 
